61a0431850f206f45686e2f193aae72aaf0abdb9
victor sanchez <victor.sanchez.mt@usa.edu.co>
2022-09-14 19:09:42 -0500

    agregando clase

---
+++ src/main/java/com/mycompany/ensayo1/Cargo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.ensayo1;
+
+/**
+ *
+ * @author pc3
+ */
+public class Cargo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola cargo"); // TODO code application logic here
+    }
+    
+}
